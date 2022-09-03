644c91aa06ac3fe07a335d4123e50619674e79e4
paulius-str <paulius.st2@gmail.com>
2022-09-03 21:22:06 +0300

    initial commit

---
+++ DepthCounter/Models/Branch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepthCounter.Models
+{
+    public class Branch
+    {
+        private List<Branch> _branches = new List<Branch>();
+
+        public List<Branch> Branches => _branches;
+    }
+}
---
+++ DepthCounter/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DepthCounter.Models;
+using DepthCounter.Tools;
+
+Console.WriteLine("Hello, World!");
+
+// Creates a new st
+Branch manuallyCreatedTree = BranchGenerator.GenerateNewTree();
+int depth = DepthCalculator.CalculateDepth(manuallyCreatedTree);
+
+Branch rootNode = new Branch();
+// populates the branch with selected depth, large numbers can cause stack overflow
+BranchGenerator.PopulateBranch(rootNode, 12, 7);
+int secondDepth = DepthCalculator.CalculateDepth(rootNode);
+
+Console.WriteLine($"Depth of the tree: {depth}");
+Console.WriteLine($"Depth of the tree: {secondDepth}");
+
